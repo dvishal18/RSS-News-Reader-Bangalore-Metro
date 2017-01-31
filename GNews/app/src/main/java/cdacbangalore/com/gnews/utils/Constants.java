33e3dd997216035c35654a3a13562f3588814428
Vishal Darekar <vish.darekar@gmail.com>
2017-01-31 15:09:29 +0530

    RSS Reader
    RSS News Reader

---
+++ GNews/app/src/main/java/cdacbangalore/com/gnews/utils/Constants.java
@@ -0,0 +1,10 @@
+package cdacbangalore.com.gnews.utils;
+
+
+public class Constants {
+
+    public static final String TOPSTORIES[] =
+            {"https://news.google.com/news?q=bangalore%20metro&output=rss" };
+//https://news.google.com/news?q=Bangalore%20metro&output=rss
+//https://news.google.com/news?cf=all&hl=en&pz=1&ned=in&q=namma+metro&cf=all&ncl=doxXk5DqJjNPxKMZniIYrgJRXwT7M&output=rss
+}
